2b432b9ac5630bce1a0f725a49ed5454c910b4c2
Alexey Grunskii <you@example.com>
2023-01-27 12:13:14 +0300

    Add getTopic

--- homework03/app/src/main/kotlin/homework03/TopicSnapshot.kt
+++ homework03/app/src/main/kotlin/homework03/TopicSnapshot.kt
@@ -1,20 +1,75 @@
 package homework03
 
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.net.URL
 
 data class TopicSnapshot (
+    @JsonAlias("created_utc")
     val timeCreate: Int,
+
+    @JsonAlias("active_user_count")
     val countOnlineSubscribers: Int,
-    val rangSize: Int,
-    val description: String
+
+    @JsonAlias("subscribers")
+    val countAllSubscribers: Int,
+
+    @JsonAlias("public_description")
+    val description: String,
+
+    var topics: List<Topic> = listOf()
 )
-// TODO: you need to understand where you may get this data
+
+data class Topic (
+    val author: String,
+
+    @JsonAlias("created_utc")
+    val timeCreate: Int,
+
+    @JsonAlias("ups")
+    val countUps: Int,
+
+    @JsonAlias("downs")
+    val countDowns: Int,
+
+    val title: String,
+
+    @JsonAlias("selftext")
+    val text: String,
+
+    @JsonAlias("selftext_html")
+    val textHTML: String
+)
+
+data class TopicsData (
+    val data: Data
+)
+
+data class InfoData (
+    @JsonAlias("data")
+    val topicSnapshot: TopicSnapshot
+)
+
+data class Data (
+    @JsonAlias("children")
+    val topics: List<Topic>
+)
+
 suspend fun getTopic(name: String): TopicSnapshot {
-    return TopicSnapshot(0, 0, 0, "")
+    val topics: List<Topic> = ObjectMapper().readValue(
+        URL("https://www.reddit.com/r/$name/.json"),
+        TopicsData::class.java
+    ).data.topics
+    val topicSnapshot: TopicSnapshot = ObjectMapper().readValue(
+        URL("https://www.reddit.com/r/$name/about.json"),
+        InfoData::class.java
+    ).topicSnapshot
+    topicSnapshot.topics = topics
+    return topicSnapshot
 }
 
-fun getJSON(name: String): String {
-    val url = URL("https://www.reddit.com/r/Kotlin/about.json")
+fun getJSON(name: String, type: String): String {
+    val url = URL("https://www.reddit.com/r/$name/$type")
     var result: String = ""
     do {
         try {
