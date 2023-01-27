d86ef7c89f6d7969227a593e1cf931c4a49c1082
Alexey Grunskii <you@example.com>
2023-01-27 16:46:35 +0300

    Add class for deserialization json

--- homework03/app/src/main/kotlin/homework03/CommentsSnapshot.kt
+++ homework03/app/src/main/kotlin/homework03/CommentsSnapshot.kt
@@ -3,21 +3,17 @@ package homework03
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class CommentsInfoWrapper(
-    @JsonProperty("data")
-    val data: BaseCommentsWrapper
-)
+data class CommentsInfoWrapper(@JsonProperty("data") val data: BaseCommentsWrapper)
 
-data class BaseCommentsWrapper(
-    @JsonProperty("children")
-    val wrapperComments: List<CommentWrapper>
-)
+data class BaseCommentsWrapper(@JsonProperty("children") val wrapperComments: List<CommentWrapper>)
 
-data class CommentWrapper(
-    @JsonProperty("data")
-    val data: Comment
-)
+data class CommentWrapper(@JsonProperty("data") val data: Comment)
 
 data class Comment(
     @JsonProperty("created_utc") val timeCreate: Int,
+    @JsonProperty("ups") val countLikes: Int,
+    @JsonProperty("downs") val countDislikes: Int,
+    @JsonProperty("body") val text: String?,
+    @JsonProperty("author") val author: String?,
+    @JsonProperty("replies") val replies: CommentsInfoWrapper?
 )
